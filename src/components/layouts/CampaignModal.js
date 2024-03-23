665a9936faaf2d2c5e4b2e399e5ad63a41dfca22
Vikas Thapa <142878082+Thapavikas@users.noreply.github.com>
2024-03-23 08:23:42 +0530

    modified

--- src/components/layouts/CampaignModal.js
+++ src/components/layouts/CampaignModal.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import './../layouts/Campaign.css'
-
-
+import './../layouts/Campaign.css';
+import camp1 from '../../img/Navbar/Navbar1.png'
+import camp2 from '../../img/Navbar/Navbar3.jpg'
+import camp3 from '../../img/Navbar/Navbar4.jpg'
 
 const CampaignModal = () => {
   return (
-    <div class='Campaign'>
-      <h3 class='Accheading'>Manual Bank Account Details </h3>
-      <div class='Accdetails'>
-        <p class='Accpara'>
+    <div className='Campaign'>
+      <h3 className='Accheading'>Manual Bank Account Details</h3>
+      <div className='Accdetails'>
+        <p className='Accpara'>
           <b>
-          Account Number : 12345678901<br/>
-          IFSC Code: SBIN1112341<br/>
-          Bank Name: State Bank of india<br/> 
-          Account type: Current<br/>
-          Branch:AustinTown Branch
+            Account Number: 12345678901<br/>
+            IFSC Code: SBIN1112341<br/>
+            Bank Name: State Bank of India<br/>
+            Account Type: Current<br/>
+            Branch: AustinTown Branch
           </b>
         </p>
       </div>
-      <div class='Acclogo'>
+      <div className='Acclogo'>
+        {/* Placeholder for bank logo */}
+        <img src={camp1}/>
       </div>
-      <h4 class='QRcod'>QR Code Scanner</h4>
-      <div class='QRcode'>
+      <h4 className='QRcod'>QR Code Scanner</h4>
+      <div className='QRcode'>
+        {/* Placeholder for QR code image */}
+        <img src={camp2}/>
       </div>
-      <p style={{textAlign:'center'}}> <b>Account Number:1234567801,IFSC Code: SBIN1112341 </b></p>
-      <p style={{textAlign:'center',textDecoration:'underline'}}> <b>Scan and Pay Using any UPI supported Apps</b></p>
-      <div class='Upisupport'></div>
+      <p style={{ textAlign: 'center' }}>
+        <b>Account Number: 1234567801, IFSC Code: SBIN1112341</b>
+      </p>
+      <p style={{ textAlign: 'center', textDecoration: 'underline' }}>
+        <b>Scan and Pay Using any UPI supported Apps</b>
+      </p>
+      <div className='Upisupport'>
+        {/* Placeholder for UPI supported apps */}
+        <img src={camp3}/>
+      </div>
     </div>
   );
 };
